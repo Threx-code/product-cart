21b669c8c02ab14405dad52b18ca447bf84cb182
Oluwatosin Amokeodo <oluwatosin.amokeodo@gmail.com>
2024-01-11 13:57:49 +0100

    Optimizing the routes

--- routes/api.php
+++ routes/api.php
@@ -19,33 +19,25 @@ use Illuminate\Support\Facades\Route;
 Route::middleware([])
     ->prefix('/v1/')
     ->group(function () {
-        Route::get('/products', [ProductsController::class, 'getAllProducts'])->name('all-products');
-        Route::get('/products/{product_id}', [ProductsController::class, 'getSingleProduct'])->name('get-a-product');
-        Route::post('/products', [ProductsController::class, 'store'])->name('store.products');
-        Route::delete('/products/{product_id}', [ProductsController::class, 'deleteAProduct'])->name('delete-a-product');
+        Route::prefix('/products')->group(function(){
+            Route::get('/', [ProductsController::class, 'getAllProducts'])->name('all-products');
+            Route::get('/{product_id}', [ProductsController::class, 'getSingleProduct'])->name('get-a-product');
+            Route::post('/', [ProductsController::class, 'store'])->name('store.products');
+            Route::delete('/{product_id}', [ProductsController::class, 'deleteAProduct'])->name('delete-a-product');
+        });
 
-        Route::get('/users', [UserController::class, 'getAllUsers'])->name('all-users');
-        Route::get('/users/{user_id}', [UserController::class, 'getSingleUser'])->name('get-a-user');
+        Route::prefix('/users')->group(function (){
+            Route::get('/', [UserController::class, 'getAllUsers'])->name('all-users');
+            Route::get('/{user_id}', [UserController::class, 'getSingleUser'])->name('get-a-user');
+        });
 
-
-        Route::get('/carts/', [CartsController::class, 'getAllCarts'])->name('all-carts');
-        Route::get('/carts/removed', [CartsController::class, 'getProductsRemovedFromCarts'])->name('get-products-removed-from-cart');
-        Route::get('/carts/{user_id}', [CartsController::class, 'getAUserCarts'])->name('get-a-user-cart');
-        Route::get('/carts/active/{user_id}', [CartsController::class, 'getAUserActiveCart'])->name('get-a-user-active-cart');
-        Route::get('/carts/removed/{user_id}', [CartsController::class, 'getAUserProductsRemovedFromCarts'])->name('get-a-user-product-removed-from-cart');
-        Route::post('/carts/', [CartsController::class, 'addToCart'])->name('add-to-cart');
-        Route::put('/carts/', [CartsController::class, 'removedFromCarts'])->name('remove-from-cart');
-
-
-
-
-
-
-//
-//        Route::middleware(['tasks.file.processor'])->group(function () {
-//            Route::post('/tasks', [TaskController::class, 'store']);
-//            Route::put('/tasks/{id}', [TaskController::class, 'update']);
-//        });
-//        Route::delete('tasks/{id}', [TaskController::class, 'delete']);
-
+        Route::prefix('/carts')->group(function (){
+            Route::get('/', [CartsController::class, 'getAllCarts'])->name('all-carts');
+            Route::get('/removed', [CartsController::class, 'getProductsRemovedFromCarts'])->name('get-products-removed-from-cart');
+            Route::get('/{user_id}', [CartsController::class, 'getAUserCarts'])->name('get-a-user-cart');
+            Route::get('/active/{user_id}', [CartsController::class, 'getAUserActiveCart'])->name('get-a-user-active-cart');
+            Route::get('/removed/{user_id}', [CartsController::class, 'getAUserProductsRemovedFromCarts'])->name('get-a-user-product-removed-from-cart');
+            Route::post('/', [CartsController::class, 'addToCart'])->name('add-to-cart');
+            Route::put('/', [CartsController::class, 'removedFromCarts'])->name('remove-from-cart');
+        });
     });
